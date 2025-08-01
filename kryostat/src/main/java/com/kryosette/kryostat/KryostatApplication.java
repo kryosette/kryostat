5c930f8cd5958ab2616ed07b5ed6c932911b8d77
dima853 <kosiktv546@gmail.com>
2025-08-01 17:05:09 +0300

    something create

--- kryostat/src/main/java/com/kryosette/kryostat/KryostatApplication.java
+++ kryostat/src/main/java/com/kryosette/kryostat/KryostatApplication.java
@@ -1,9 +1,22 @@
 package com.kryosette.kryostat;
 
+import com.kryosette.storage.config.StorageConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import({
+		StorageConfig.class,
+})
+@ComponentScan(excludeFilters = @ComponentScan.Filter(
+		type = FilterType.ASSIGNABLE_TYPE,
+		classes = {
+				StorageConfig.class
+		}
+))
 public class KryostatApplication {
 
 	public static void main(String[] args) {
