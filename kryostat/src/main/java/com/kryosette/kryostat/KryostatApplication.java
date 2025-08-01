f6436a80c2cbd63459904f66e82e8d3acbc9a287
dima853 <kosiktv546@gmail.com>
2025-08-01 16:30:15 +0300

    something create

---
+++ kryostat/src/main/java/com/kryosette/kryostat/KryostatApplication.java
@@ -0,0 +1,13 @@
+package com.kryosette.kryostat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KryostatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KryostatApplication.class, args);
+	}
+
+}
---
+++ kryostat/src/test/java/com/kryosette/kryostat/KryostatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kryosette.kryostat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KryostatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
