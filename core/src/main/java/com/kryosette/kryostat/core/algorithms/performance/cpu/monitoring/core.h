da80347edfc739c0049340ef4cdf88fb2cc4f749
dima853 <kosiktv546@gmail.com>
2025-08-02 21:23:28 +0300

    something create

--- core/src/main/java/com/kryosette/kryostat/core/algorithms/performance/cpu/monitoring/core.h
+++ core/src/main/java/com/kryosette/kryostat/core/algorithms/performance/cpu/monitoring/core.h
@@ -35,6 +35,35 @@ struct ethheader
     uint8_t hort ether_type; // Protocol type (IPv4/IPv6)
 };
 
+/*
+Why is this needed in the IP header? (bit fields)
+
+The IPv4 header has a Fragment Offset field, which consists of:
+
+    3 bits of flags (flag):
+
+        Bit 0: reserved (always 0)
+
+        Bit 1: DF (Don't Fragment) — no fragmentation
+
+        Bit 2: MF (More Fragments) — there are more fragments
+
+    13 offset bits:
+
+        Specifies the fragment's position in the source packet (in blocks of 8 bytes)
+
+An example from a real IP packet:
+text
+
+Flags: 0x2000 (Don't Fragment)
+Fragment Offset: 0
+
+    Here is 0x2000 in bits: 001 0000000000000
+
+        001 — flags (DF=1, MF=0)
+
+        0000000000000 — offset = 0
+*/
 // IP header structure (standard 20-byte header)
 struct ipheader
 {
